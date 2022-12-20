ceb780edb7269a8df429975bc7f000b7b968f5d4
Matthew Hanson <108187519+mattHanson733@users.noreply.github.com>
2022-12-20 00:28:09 +0000

    finished project

--- script.js
+++ script.js
@@ -1,178 +1,184 @@
-document.addEventListener('DOMContentLoaded', buzzfeedQuiz, false);
+const displayResult = document.querySelector(".display-result");
+const submitButton = document.querySelector(".submit-btn");
+
+let q1 = document.querySelector(".q1");
+let q2 = document.querySelector(".q2");
+let q3 = document.querySelector(".q3");
+let q4 = document.querySelector(".q3");
+
+let q1Answer;//Set with Question #1 value
+let q2Answer; //Set with Question #2 value
+let q3Answer; //Set with Question #3 value
+let q4Answer; //Set with Question #4 value
 
 
-const Animals = {
-  "cheetah": {
-    name: "Cheetah",
-    imgSource: "http://cdn.shopify.com/s/files/1/1951/2637/articles/082119_Cheetah_BlogPost_featured-image.jpg?v=1566407036"
-  },
-  "mouse": {
-    name: "Mouse",
-    imgSource: "https://a-z-animals.com/media/2019/11/Mouse-header.jpg"
-  },
-  "owl": {
-    name: "Owl",
-    imgSource: "https://www.nps.gov/articles/000/images/GGO_spread-wings_Mel-Clements.jpg?maxwidth=1200&autorotate=false"
-  },
-  "shark": {
-    name: "Shark",
-    imgSource: "https://cdn.britannica.com/79/65379-050-5CF52BAC/Shortfin-mako-shark-seas.jpg"
-  },
-  "bear": {
-    name: "Bear",
-    imgSource: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/1200px-2010-kodiak-bear-1.jpg"
-  },
-  "moose": {
-    name: "Moose",
-    imgSource: "https://cdn.britannica.com/57/92857-050-8D5A0A8E/bull-moose-water.jpg"
-  },
+const API_ENDPOINT_URL = "https://api.random.org/json-rpc/4/invoke";
+const API_KEY = "cd5f8d1f-153c-484e-8d90-b14b314fee36";
+
+const getRandomChance = (lowerBound, upperBound) => {
+
+  fetch(API_ENDPOINT_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'generateIntegers',
+      params: {
+        "apiKey": API_KEY,
+        "n": 1,
+        "min": lowerBound,
+        "max": upperBound,
+        "replacement": false
+      },
+      id: 1,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      return data.result.random.data[1];
+    });
 }
 
+const getUserAnswerScore = () => {
+  console.log("Got to getUserAnswerScore");
 
-const getSpecialChance = () => {
-  let a = Math.floor(Math.random() * 101);
-  if (a >= 75) {
-    return 1;
-  } else if (a > 95) {
-    return 2;
+  let questionScores = [];
+
+  if (q1Answer === "morning") {
+    questionScores.push(1);
+  } else if (q1Answer === "afternoon") {
+    questionScores.push(2);
+  } else if (q1Answer === "evening") {
+    questionScores.push(3);
+  } else if (q1Answer === "night") {
+    questionScores.push(4);
   }
 
-  return 0;
+
+  if (q2Answer === "salad") {
+    questionScores.push(4);
+  } else if (q2Answer === "sushi") {
+    questionScores.push(3);
+  } else if (q2Answer === "chicken salad") {
+    questionScores.push(2);
+  } else if (q2Answer === "pizza") {
+    questionScores.push(1);
+  }
+
+
+  if (q3Answer === "track and field") {
+    questionScores.push(1);
+  } else if (q3Answer === "football") {
+    questionScores.push(2);
+  } else if (q3Answer === "soccer") {
+    questionScores.push(3);
+  } else if (q3Answer === "tennis") {
+    questionScores.push(4);
+  }
+
+
+  if (q4Answer === "freight") {
+    questionScores.push(4);
+  } else if (q4Answer === "bullet") {
+    questionScores.push(3);
+  } else if (q4Answer === "light rail") {
+    questionScores.push(2);
+  } else if (q4Answer === "none") {
+    questionScores.push(1);
+  }
+
+  return questionScores;
 }
 
-const processAnswerChoices = () => {
-  if (q1Choice === 'Morning') {
-    quizAnswers.push(1);
-  } else if (q1Choice === 'Afternoon') {
-    quizAnswers.push(2);
-  } else if (q1Choice === 'Evening') {
-    quizAnswers.push(3);
-  } else if (q1Choice === 'Night') {
-    quizAnswers.push(4);
+const calcUserScore = (scoreArray) => {
+
+  let totalScore = 0;
+
+  for (let i = 0; i < scoreArray.length; i++) {
+    totalScore += scoreArray[i];
   }
 
-
-  if (q2Choice === 'Salad') {
-    quizAnswers.push(1);
-  } else if (q2Choice === 'Sushi') {
-    quizAnswers.push(2);
-  } else if (q2Choice === 'Chicken Salad') {
-    quizAnswers.push(3);
-  } else if (q2Choice === 'Pizza') {
-    quizAnswers.push(4);
-  }
-
-
-  if (q3Choice === 'Track and Field ') {
-    quizAnswers.push(1);
-  } else if (q3Choice === 'Soccer') {
-    quizAnswers.push(2);
-  } else if (q3Choice === 'Football') {
-    quizAnswers.push(3);
-  } else if (q3Choice === 'Tennis') {
-    quizAnswers.push(4);
-  }
-
-
-  if (q4Choice === 'Freight') {
-    quizAnswers.push(1);
-  } else if (q4Choice === 'Bullet') {
-    quizAnswers.push(2);
-  } else if (q4Choice === 'Light Rail') {
-    quizAnswers.push(3);
-  } else if (q4Choice === 'None') {
-    quizAnswers.push(4);
-  }
+  return totalScore;
 }
 
-
-const evaluateFinalScore = () => {
-
-  if (4 <= X < 8) {
-    outputResult('cheetah');
-  } else if (8 <= X < 12) {
-    outputResult('mouse');
-  } else if (12 <= X < 16) {
-    outputResult('owl');
-  } else if (16 = X) {
-    outputResult('shark');
-  }
-
-}
-
-const outputResult = (animalResult) => {
-  displayResult.insertAdjacentElement(beforeend, `You are a(n) ${Animal.animalResult.name}`);
-  displayResult.insertAdjacentElement(beforeend, `<img class="result-img" src="${Animal.animalResult.img}">`);
-}
-
-function buzzfeedQuiz() {
-
-  // task 4: declare variables for the html elements you'll use as selectors later. You need the submit button, display div, and all three inputs for your questions.
-
-  let button = document.querySelector(".submit");
-  let displayResult = document.querySelector(".displayResult");
-  let q1 = document.querySelector(".q1");
-  let q2 = document.querySelector(".q2");
-  let q3 = document.querySelector(".q3");
-  let q4 = document.querySelector(".q3");
-
-  // task 5: declare variables for values we'll set later. You'll need one for an image source, a result and a score for each question.
-  let resultImg = "";
-  let quizResult = 0;
-  let questionScoreTotal = 0;
-  let quizAnswers = [];
-  let resultAnimal = "";
-
-  quizAnswers
+const elvalutateFinalScore = (finalScore) => {
+  let randomChanceNumber = getRandomChance(1, 100);
 
 
-  button.onclick = function() {
-
-    // task 6: Declare user input variables for each of your questions. If you have three questions, you should have three variables. These variables should equal the value of the user input for each question.
-    let q1Choice = q1.value.toLowerCase();
-    let q2Choice = q2.value.toLowerCase();
-    let q3Choice = q3.value.toLowerCase();
-    let q4Choice = q4.value.toLowerCase();
-
-    let randomBonus = getSpecialChance();
-
-    if (randomBonus != 0) {
-      if (randomBonus === 1) {
-        resultAnimal = "Bear";
-      } else if (randomBonus === 2) {
-        resultAnimal = "moose";
-      }
-    } else {
-      evaluateFinalScore(quizAnswers);
+  if (randomChanceNumber >= 75 && randomChanceNumber <= 95) {
+    return {
+      name: "Bear",
+      imgSource: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/2010-kodiak-bear-1.jpg/1200px-2010-kodiak-bear-1.jpg"
+    };
+  } else if (randomChanceNumber > 95) {
+    return {
+      name: "Moose",
+      imgSource: "https://cdn.britannica.com/57/92857-050-8D5A0A8E/bull-moose-water.jpg"
+    };
+  } else {
+    if (finalScore >= 4 && finalScore < 8) {
+      return {
+        name: "Cheetah",
+        imgSource: "http://cdn.shopify.com/s/files/1/1951/2637/articles/082119_Cheetah_BlogPost_featured-image.jpg?v=1566407036"
+      };
+    } else if (finalScore >= 8 && finalScore < 12) {
+      return {
+        name: "Mouse",
+        imgSource: "https://a-z-animals.com/media/2019/11/Mouse-header.jpg"
+      };
+    } else if (finalScore >= 12 && finalScore < 16) {
+      return {
+        name: "Owl",
+        imgSource: "https://www.nps.gov/articles/000/images/GGO_spread-wings_Mel-Clements.jpg?maxwidth=1200&autorotate=false"
+      };
+    } else if (finalScore === 16) {
+      return {
+        name: "Shark",
+        imgSource: "https://cdn.britannica.com/79/65379-050-5CF52BAC/Shortfin-mako-shark-seas.jpg"
+      };
     }
-
-
-    // task 7: create a conditional statement for the responses to Question 1 based on the user input.
-
-
-
-
-    // task 8: create a conditional statement for the responses to Question 2 based on the user input.
-
-
-
-
-    // task 9: create a conditional statement for the responses to Question 3 based on the user input.
-
-
-
-
-    // task 10: declare a variable for the total score of the quiz and set it equal to the sum of each question score.
-
-
-
-
-    // task 11: create a conditional statement for the result to the quiz based on the total score. Using string interpolation, display these to the page: the result, an image that corresponds and a message to go with it.
-
-
-
-
-
-
-  };
+  }
 }
+
+const processAnswers = () => {
+  let userAnswerArr = getUserAnswerScore();
+
+  let finalScore = calcUserScore(userAnswerArr);
+
+  return elvalutateFinalScore(finalScore);
+}
+
+const getQuizResult = () => {
+  q1Answer = q1.value; //Set with Question #1 value
+  q2Answer = q2.value; //Set with Question #2 value
+  q3Answer = q3.value; //Set with Question #3 value
+  q4Answer = q4.value; //Set with Question #4 value
+
+
+  let resultAnimal = processAnswers();
+console.log("quizResult finish process")
+  
+  displayResult.insertAdjacentHTML("beforeend", `<p class='result-text'>You are a(n) ${resultAnimal.name}</p>`);
+  
+  displayResult.insertAdjacentHTML("beforeend", `<img class='result-img' src='${resultAnimal.imgSource}'>`);
+}
+
+/*
+submitButton.onclick(() => {
+  q1Answer = q1.value; //Set with Question #1 value
+  q2Answer = q2.value; //Set with Question #2 value
+  q3Answer = q3.value; //Set with Question #3 value
+  q4Answer = q4.value; //Set with Question #4 value
+
+  let resultAnimal = processAnswers();
+
+  displayResult.insertAdjacentElement("beforeend", `<p class='result-text'>You are a(n) ${resultAnimal.name}</p>`);
+  console.log("outputted name");
+  
+  displayResult.insertAdjacentElement("beforeend", `<img class='result-img' href='${resultAnimal.imgSource}'>`);
+
+});
+*/
